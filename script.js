4dc06708f1885108fb59dc2beb75315c025753f9
hardikk04 <hardik04062003@gmail.com>
2024-02-06 15:26:54 +0530

    page1 all done

--- script.js
+++ script.js
@@ -13,3 +13,48 @@ function lenisJs() {
 }
 
 lenisJs();
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".page1-part2",
+    start: "top -20%",
+    end: "top -200%",
+    scrub: 1,
+    pin: true,
+    markers: true,
+  },
+});
+
+tl.to(
+  ".page1-part2-center",
+  {
+    width: "32%",
+    height: "90vh",
+  },
+  "a"
+);
+
+tl.to(
+  ".page1-part2-center>img",
+  {
+    scale: 1,
+  },
+  "a"
+);
+
+tl.to(
+  ".page1-part2-left",
+  {
+    top: "2.9%",
+  },
+  "b"
+);
+
+tl.to(
+  ".page1-part2-right",
+  {
+    top: "24.5%",
+  },
+  "b"
+);
